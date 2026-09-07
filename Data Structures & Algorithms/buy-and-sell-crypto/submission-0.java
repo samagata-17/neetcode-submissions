4ad9032e0a9df15c98ef6a279f214967dcf760b5
samagata-17 <samagatamishra51@gmail.com>
2026-09-07 11:58:35 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for(int i = 0 ; i < prices.length ; i++){
+            minPrice = Math.min(prices[i],minPrice);
+            int profit = prices[i] - minPrice;
+            maxProfit = Math.max(maxProfit,profit);
+        }
+        return maxProfit;
+    }
+}
